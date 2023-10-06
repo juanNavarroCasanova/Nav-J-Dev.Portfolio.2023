ca141ce5abb9ccae07f553d7945f233da2ff14c9
navakasa1979 <juan.casanova@ausloans.com.au>
2023-10-07 06:56:36 +1000

    add content to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <div class="flex items-start w-[90vw] lg:max-w-[60vw] pl-0 pt-[20vh]">
+        <div class="flex items-start w-[90vw] lg:max-w-[60vw] pl-0 pt-[15vh]">
             <Typewriter :data="data"/>
         </div>
     </section>
@@ -8,6 +8,8 @@
 <script setup>
 const data = [
   'Hi, I am Juan Navarro Casanova',
-  'Built with Vue 3, TypeScript, & Tailwind'
+  'Software Developer focusing on Vue, TypeScript & Tailwind',
+  'With experience in React, Angular 6/7, Node Js and Laravel',
+  'Versatile developer ready to tackle a wide range of challenges',
 ];
 </script>
