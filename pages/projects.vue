cafde7ebc1e491fb4febfcd218a697160fb808cc
navakasa1979 <juan.casanova@ausloans.com.au>
2023-10-07 06:44:34 +1000

    projects page mobile

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,17 @@
 <template>
-    <h1 class="text-3xl my-8">Projects</h1>
-    <p class="text-lg mb-8">Here are some of my projects on GitHub.</p>
-    <section class="grid grid-cols-2 gap-10">
+    <h1 class="text-3xl font-bold my-8">Projects</h1>
+    <p class="text-lg mb-8 font-semibold italic">Here are some of my projects on GitHub.</p>
+    <section class="grid lg:grid-cols-2 gap-10">
       <div v-for="project in data?.viewer.repositories.nodes" :key="project.id"
-        class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
+        class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50 border-gray-300">
         <a :href="project.url" target="_blank">
-          <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">{{ project.name }}</h2>
+          <h2 class="text-2xl text-black font-semibold mb-2 hover:text-3xl">{{ project.name }}</h2>
         </a>
         <p>{{ project.description }}</p>
         <div class="mt-4">
-          <Icon name="fontisto:star" size="1.1rem" class="text-indigo-700" /> Stars: {{ project.stargazers.totalCount }}
-          <Icon name="system-uicons:branch" size="1.1rem" class="text-indigo-800" /> Forks: {{ project.forks.totalCount }}
-          <Icon name="system-uicons:eye" size="1.1rem" class="text-indigo-700" /> Watchers: {{
+          <Icon name="fontisto:star" size="1.1rem" class="text-pink-600" /> Stars: {{ project.stargazers.totalCount }}
+          <Icon name="system-uicons:branch" size="1.1rem" class="text-pink-600" /> Forks: {{ project.forks.totalCount }}
+          <Icon name="system-uicons:eye" size="1.1rem" class="text-pink-600" /> Watchers: {{
             project.watchers.totalCount }}
         </div>
       </div>
