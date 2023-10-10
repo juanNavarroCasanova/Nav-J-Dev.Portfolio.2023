d4d789646822d5ff621610db79c9b7504c10df7d
navakasa1979 <juan.casanova@ausloans.com.au>
2023-10-10 10:47:54 +1000

    add meta and plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,17 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon'
-  ]
+  ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+      meta: [
+        { property: 'og:title', content: 'Nav-J Development' },
+        { property: 'og:description', content: 'Software developer based in Australia' },
+        { property: 'og:url', content: 'https://nav-j-dev.com/' },
+      ],
+    }
+  }
 })
